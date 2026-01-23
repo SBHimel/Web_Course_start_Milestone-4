46e20534d5b5907d357de282188cc2a5daded6e6
SBHimel <s.b.himel21@gmail.com>
2026-01-23 22:47:44 +0600

    done

---
+++ Milestone-4/Module-17-h-13/17-13-practice-task.js
@@ -0,0 +1,31 @@
+var orange = 100;
+var apples = 250;
+var total = orange + apples;
+console.log(total);
+var Pay = 500;
+console.log(Pay-total);
+
+// Task 2
+
+var bangla = 87;
+var english = 80;
+var math = 92;
+var science = 88;
+var islam = 85;
+var somaj = 93;
+var TotalMark = bangla+english+math+science+islam+somaj;
+Average = TotalMark/6;
+console.log(TotalMark);
+console.log(Average);
+console.log(Average.toFixed(3));
+
+// Task three
+
+var firstNumber = 119;
+var secondNumber = 4;
+console.log(firstNumber % secondNumber);
+
+const a1 = isNaN("11");
+const a2 = isNaN(2-10);
+
+console.log(a1, a2); // false false
---
+++ Milestone-4/Module-18-h-14/18-h-14-2-introdu-to-condition.js
@@ -0,0 +1,12 @@
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(5 == 5);
+
+var peyara = 40;
+var apple = 240;
+console.log(peyara > apple);
+console.log(peyara == apple);
+//  ===
+console.log(10 == "10")
+console.log(10 === "10")
+console.log(10 != 10);
