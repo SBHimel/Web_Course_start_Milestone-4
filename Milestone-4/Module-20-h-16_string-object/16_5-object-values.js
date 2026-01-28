07d78c519386e676a903d63c2303491244aa78d3
SBHimel <s.b.himel21@gmail.com>
2026-01-28 23:56:00 +0600

    done

--- Milestone-4/Module-20-h-16_string-object/16_5-object-values.js
+++ Milestone-4/Module-20-h-16_string-object/16_5-object-values.js
@@ -3,7 +3,7 @@ const school = "rifle square"
 const isPassed = true;
 let isDeveloper;
 const subjects = ["bangla", "English", "Physics", "Math"]
-const bottle = ["white", 45];
+const bottlee = ["white", 45];
 const bottlePrice = 45;
 const bottleDrinks = water;
 
@@ -11,14 +11,14 @@ const bottleDrinks = water;
 const bottle = {
     brand: "apple",
     price: 45,
-    color: ""white",
-    isClean : false;
+    color: "white",
+    isClean : false,
 }
 const subject = {
     name: "biology",
     teacher: "rasheda mam",
-    examDate: "30 Feb"
-    chapter: ["first", 'second', 'third'],
+    examDate: "30 Feb",
+    chapter : ["first", 'second', 'third'],
     exams: {
         name: 'final Exam',
         marks : 100
