2b72540cb5946fdb3d5ea1a2371cdd94cb6f1127
SBHimel <s.b.himel21@gmail.com>
2026-02-02 23:04:12 +0600

    done

--- Milestone-4/Module-20-h-16_string-object/16_9-ways-to-declare-an-object.js
+++ Milestone-4/Module-20-h-16_string-object/16_9-ways-to-declare-an-object.js
@@ -6,7 +6,24 @@ const mobile = {
     camera: '12mp',
     isNew: true,
 }
+// for of : array  er moddhe calai
+// for in : onject  er moddhe calai
 
 for(const prop in mobile){
-    console.log(prop)
+    // console.log(prop)
+    // console.log(mobile[prop])
 }
+// another ways
+
+const keys = Object.keys(mobile);
+console.log(keys);
+for(const key of keys){
+    console.log(key, ':', mobile[key])
+}
+
+//  Object declare another ways
+const pen = { brand: "econo", price: 10, colore: "bkack"}
+const pencil = new Object()
+console.log(pencil)
+const rubber = Object.create({})
+console.log(rubber)
