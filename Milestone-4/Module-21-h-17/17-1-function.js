ac597feec4106abd2ccf22fb5d75b689a927d257
SBHimel <s.b.himel21@gmail.com>
2026-02-04 16:16:25 +0600

    done

---
+++ Milestone-4/Module-21-h-17/17-1-function.js
@@ -0,0 +1,29 @@
+
+
+const price = 45;
+
+function faOfKor(){
+    console.log("bosa theke uthe dara")
+    console.log("walk towards the switch")
+    console.log("click the switch to off of the Fan")
+}
+/*
+ function functionName() {
+    // code here
+}
+ */
+function brushTeeth(){
+    console.log("pick up the brush");
+    console.log("add past");
+    console.log("2 min gosa goshi koro");
+}
+
+// call the function
+faOfKor();
+brushTeeth();
+brushTeeth();
+console.log('-----------');
+console.log('-----------');
+console.log('-----------');
+console.log('-----------');
+brushTeeth();
---
+++ Milestone-4/Module-21-h-17/17-3-function-paramiter-mutiple-parameters.js
@@ -0,0 +1,40 @@
+
+function vaatKhao() {
+    console.log("hand wash kore aso");
+    console.log("boso");
+    console.log("plate e khabar dhabar nao");
+    console.log("thas thos khawa shuru koro")
+}
+vaatKhao();
+
+
+
+// 3 er borgo --> 3*3 = 9
+// 4 --> 4*4 = 16
+
+function square(number) {
+    console.log("value of the number parameter", number);
+    const borgo = number * number;
+    console.log("square of the givn number is: ", borgo);
+}
+square(4);
+console.log("---------");
+square(12);
+square(405);
+
+
+//  add kora 
+function add(num1, num2) {
+    const sum = num1 + num2;
+    console.log(sum);
+}
+add(5, 71);
+add(60, 30);
+
+
+
+function addAll(a, b, c, d, e) {
+    const total = a + b + c + d + e;
+    console.log(total);
+}
+addAll(10,20,30,40,50);
