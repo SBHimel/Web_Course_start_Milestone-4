736bafb532130fc500267c974a621809ef40532b
SBHimel <s.b.himel21@gmail.com>
2026-02-12 12:25:48 +0600

    done

--- Milestone-4/Module-24.5-h-20.5/20_5-2find.js
+++ Milestone-4/Module-24.5-h-20.5/20_5-2find.js
@@ -11,6 +11,7 @@ for (const number of numbers) {
 }
 
 
+// -----ফোনগুলোর একটি array (array of objects)
 
 const phones = [
     { name: "Samsung", price: 20000, camera: "12mp", color: "black" },
@@ -19,4 +20,20 @@ const phones = [
     { name: "Iphone", price: 100000, camera: "12mp", color: "black" },
     { name: "Walton", price: 31000, camera: "12mp", color: "black" },
     { name: "HTC", price: 7000, camera: "12mp", color: "black" },
-]
+];
+
+function matchedProducts(phones, search){
+    const matched = []; // match হওয়া ফোন রাখা হবে এখানে
+
+    for(const phone of phones){
+        // search word নামের মধ্যে আছে কি check
+        if(phone.name.toLowerCase().includes(search.toLowerCase())){
+            matched.push(phone); // match হলে array তে push
+        }
+    }
+
+    return matched; // matched phones return
+}
+
+const result = matchedProducts(phones, "ton"); // name e thaka data er moddhe kotota ton ache ta khuje ber kore
+console.log(result); // output দেখাবে [{ name: "Iphone", ... }]
