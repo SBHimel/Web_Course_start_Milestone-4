91bfcd0161c8cd9357d6906a03662424b5eedc7c
SBHimel <s.b.himel21@gmail.com>
2026-02-19 16:38:54 +0600

    done

--- Milestone-4/Module-22/22-1-dom-start-3.js
+++ Milestone-4/Module-22/22-1-dom-start-3.js
@@ -1,2 +1,16 @@
-console.log("Hello from outside");
-console.log(document);
+// const section = document.getElementsByTagName('section');
+const sections = document.querySelectorAll('section')
+// console.log(section);
+// for(const section of sections){
+//     console.log(section);
+//     section.style.backgroundColor = 'lightblue';
+//     section.style.border = '3px solid green';
+//     section.style.marginBottom = '5px';
+//     section.style.borderRadius = '15px';
+//     section.style.padding = '10px';
+// }
+
+// dynami class-------------------
+for(const section of sections){
+    section.classList.add('section-card');
+}
