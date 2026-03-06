0b2b5683f888e3a745884d2ba34488c46feb7ba5
SBHimel <s.b.himel21@gmail.com>
2026-03-06 14:56:24 +0600

    done

---
+++ Milestone-5/Module-29/english-janala.js
@@ -0,0 +1,159 @@
+
+//  ======password er eye control====
+function togglePassword() {
+  const password = document.getElementById("password");
+  const icon = document.getElementById("eyeIcon");
+
+  if (password.type === "password") {
+    password.type = "text";
+    icon.classList.remove("fa-eye");
+    icon.classList.add("fa-eye-slash");
+  } else {
+    password.type = "password";
+    icon.classList.remove("fa-eye-slash");
+    icon.classList.add("fa-eye");
+  }
+}
+
+
+const loadLessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all") // promise of response
+    .then((res) => res.json()) // promise of json data
+    .then(json => displayLesson(json.data));
+};
+
+const removeActive = () => {
+  const lessonButtons = document.querySelectorAll(".lesson-btn");
+  // console.log(lessonButtons);
+  lessonButtons.forEach((btn) => btn.classList.remove("active"));
+};
+
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      removeActive();  //remove all active class
+      const clickBtn = document.getElementById(`lesson-btn-${id}`)
+      // console.log(clicBtn);
+      clickBtn.classList.add("active"); //add active class
+      displayLevelWord(data.data)
+    })
+};
+
+/* 
+{
+    "word": "Cautious",
+    "meaning": "সতর্ক",
+    "pronunciation": "কশাস",
+    "level": 2,
+    "sentence": "Be cautious while crossing the road.",
+    "points": 2,
+    "partsOfSpeech": "adjective",
+    "synonyms": [
+        "careful",
+        "alert",
+        "watchful"
+    ],
+    "id": 3
+}
+     */
+const loadWordDetail = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  console.log(url);
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.data);
+};
+const displayWordDetails = (word) => {
+  console.log(word);
+  const detailsBox = document.getElementById("details-container");
+  detailsBox.innerHTML = `
+   <div class="">
+                    <h2 class="text-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone-lines"></i> :${word.pronunciation})</h2>
+                </div>
+                <div class="">
+                    <h2 class=" font-bold">Meaning</h2>
+                    <p>${word.meaning}</p>
+                </div>
+                <div class="">
+                    <h2 class=" font-bold">${word.sentence}</h2>
+                    <p>The kids were eager to open their gifts.</p>
+                </div>
+                <div class="">
+                    <h2 class=" font-bold">সমার্থক শব্দ গুলো</h2>
+                    <span class="btn">Enthusiastic</span>
+                    <span class="btn">excited</span>
+                    <span class="btn">keen</span>
+                </div>
+                `;
+  document.getElementById("word_modal").showModal();
+
+}
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+    <div class="text-center col-span-full rounded-xl py-10 space-y-6 font-bangla">
+
+            <img class="mx-auto" src="english-janala-resources/assets/alert-error.png" alt="">
+            <p class=" text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+        </div>
+        `;
+    return;
+  }
+
+  //     {
+  //     "id": 82,
+  //     "level": 1,
+  //     "word": "Car",
+  //     "meaning": "গাড়ি",
+  //     "pronunciation": "কার"
+  // }
+
+  words.forEach(word => {
+    console.log(word);
+    const card = document.createElement("div");
+    card.innerHTML = `
+      <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
+            <p class="font-semibold">Meaning /Pronounciation</p>
+
+            <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায় নি"}"</div>
+            <div class="flex justify-between items-center">
+                <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+      `;
+    wordContainer.append(card);
+  })
+};
+
+const displayLesson = (lessons) => {
+  // 1.- get the container and empty
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+
+  // 2. get into every lessons
+  for (let lesson of lessons) {
+    // 3. create element
+    // console.log(lesson);
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+        <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn">
+        <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
+        </button>
+        `
+    // 4. append into container
+
+    levelContainer.append(btnDiv);
+  }
+
+}
+loadLessons();
+
