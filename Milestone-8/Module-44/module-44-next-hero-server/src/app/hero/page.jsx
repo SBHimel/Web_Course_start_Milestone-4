96fc0e15cb7c22ab86664884471a681ea3978c3c
SBHimel <s.b.himel21@gmail.com>
2026-04-23 16:56:20 +0600

    module-43

---
+++ Milestone-8/Module-44/module-44-next-hero-server/src/app/hero/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@heroui/react";
+
+const HeroPage = () => {
+    return (
+        <div>
+            <h2>Hero page</h2>
+            <Button variant="danger-soft">Hero Button</Button>
+        </div>
+    );
+};
+
+export default HeroPage;
